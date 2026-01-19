b5676467dc419ce2cbdbc866bce63f9e978ef3d9
Kashis Makwana <dolumakwana93@gmail.com>
2026-01-20 00:47:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -12,10 +11,5 @@ export default defineConfig({
       '/api': 'http://localhost:4000',
     },
   },
-  // Base path configuration - use '/' for Netlify, Vercel
-  // For GitHub Pages, use '/AluVerse-project/'
- base: process.env.NODE_ENV === 'production'
-  ? '/hackaluverse93/'
-  : '/',
-
-});
+  base: '/', // ✅ FIXED for Vercel
+})
